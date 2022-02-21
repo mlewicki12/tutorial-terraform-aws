56dd8eaf1615e7f78ade906b3b4afae284ee0b58
Maciej Lewicki <lewickimapl@gmail.com>
2022-02-21 13:58:44 +0100

    initial commit + terraform setup

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Terraform Tutorial
+
+Getting a grasp on Terraform with provided HashiCorp AWS tutorial
+https://learn.hashicorp.com/tutorials/terraform/infrastructure-as-code?in=terraform/aws-get-started
---
+++ main.tf
@@ -0,0 +1,27 @@
+
+terraform { // settings block
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" // reference to the terraform registry by default
+      version = "~> 3.27"       // version constraint to avoid breaking changes
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {      // configures specificied provider
+  profile = "default" // refers to aws credentials
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" { // component of the infrastucture: resource <type> <name>
+  // creates a unique id for the resource -> aws_instance.app_server
+  // arguments to configure the resource
+  ami           = "ami-0001321d" // ubuntu image
+  instance_type = "t2.micro"     // aws free tier
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
